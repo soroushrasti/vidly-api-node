63177481c1a2c763c2aa20b13bd4a0f919f34257
Soroush Rasti <rasti.soroush@gmail.com>
2021-06-19 16:13:09 +0200

    preparing the app for integration test

--- index.js
+++ index.js
@@ -12,7 +12,7 @@ require("./startup/validation")();
 
 const port = process.env.PORT || config.get("port");
 const server = app.listen(port, () =>
-  winston.info(`Listening on port ${port}...`)
+  winston.info(`Listening on port ${port} on ${config.get("db")} ...`)
 );
 
 module.exports = server;
